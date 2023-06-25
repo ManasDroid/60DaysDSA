c1ae5f4e16955e87fe34e248e6915fcb048a3abb
Manas Ranjan Patra <53658835+ManasDroid@users.noreply.github.com>
2023-06-25 21:10:19 +0530

    Day 6 : fifth commit

---
+++ Day6/CycleDetection2.java
@@ -0,0 +1,36 @@
+package Day6;
+
+public class CycleDetection2 {
+    class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+
+    public class Solution {
+        public ListNode detectCycle(ListNode head) {
+            if (head == null || head.next == null) return null;
+
+            ListNode slow = head, fast = head, entry = head;
+
+            while(fast.next != null && fast.next.next != null){
+                slow = slow.next;
+                fast = fast.next.next;
+                if(slow == fast){
+                    while(entry != slow){
+                        slow = slow.next;
+                        entry = entry.next;
+
+                    }
+                    return slow;
+                }
+
+            }
+
+            return null;
+        }
+    }
+}
