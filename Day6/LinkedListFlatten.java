f55017692e22ca9c6bb664443449a0d0e3a11676
Manas Ranjan Patra <53658835+ManasDroid@users.noreply.github.com>
2023-06-26 22:10:25 +0530

    Day 6 : sixth commit

---
+++ Day6/LinkedListFlatten.java
@@ -0,0 +1,40 @@
+package Day6;
+
+public class LinkedListFlatten {
+    public static Node merge(Node first, Node second) {
+        // If the first is null return second
+        if (first == null) {
+            return second;
+        }
+
+        // If the second is null return first
+        if (second == null) {
+            return first;
+        }
+
+        Node merged;
+
+        if (first.data < second.data) {
+            merged = first;
+            merged.child = merge(first.child, second);
+        } else {
+            merged = second;
+            merged.child = merge(first, second.child);
+        }
+        return merged;
+    }
+
+    public static Node flattenLinkedList(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // Recur on next node
+        head.next = flattenLinkedList(head.next);
+
+        // Merge with the current
+        head = merge(head, head.next);
+
+        return head;
+    }
+}
